c86be1817efaccba056b81156b850acf6068c4ea
Jair <dev.lnx1337@gmail.com>
2013-10-14 16:42:48 -0500

    cambios

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -126,8 +126,8 @@ from tbl_usuarios_has_tbl_comics c inner join tbl_usuarios b on b.id = c.tbl_usu
     ));
    
 
-   print_r($facebook);
     $user =$facebook->getUser();
+    print_r($user);
     $album_name = 'MIS MEMES ESPECIAL';
     $album_description = '';
     $album_id = 'blank';
