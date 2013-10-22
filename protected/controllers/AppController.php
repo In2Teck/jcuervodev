eb169fdb0564fc810faf4c59367c3a0ae242cca5
Jair <dev.lnx1337@gmail.com>
2013-10-21 22:39:08 -0500

    cambios

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -265,8 +265,8 @@ from tbl_usuarios_has_tbl_comics c inner join tbl_usuarios b on b.id = c.tbl_usu
                                       if($model->validate() && $model->login()){
                                         //echo "entro";
                                         
-                                          print_r(Yii::app()->user);
-                                         //$this->redirect(array('App/Profile/'.$user_profile['id']));
+                                         // print_r(Yii::app()->user);
+                                         $this->redirect(array('App/Profile/'.$user_profile['id']));
                                       }else{
 
                                         echo "not log";
