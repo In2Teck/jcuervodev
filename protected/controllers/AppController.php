0fa19d36006bbdfd220246ace74b94a762c1e906
Jair <dev.lnx1337@gmail.com>
2013-10-21 22:57:08 -0500

    cambios

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -21,7 +21,7 @@ class AppController extends Controller
       ),
       array('allow', // allow authenticated user to perform 'create' and 'update' actions
         'actions'=>array('create','update','create','profile','UpdatePieza','CrearAvatar','UpdateTipoPieza','MisMemes','MisAmigos','Categoria','Dest','Catmasvist','Catmascomp','Catjoscuer','Catmascome','Detalle','F'),
-        'users'=>array('@'),
+        'users'=>array('admin'),
       ),
       array('allow', // allow admin user to perform 'admin' and 'delete' actions
         'actions'=>array('admin','delete','index'),
