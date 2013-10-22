c81acb98191b44e705687893d5edb336bba91ec5
Jair <dev.lnx1337@gmail.com>
2013-10-21 21:40:47 -0500

    cambos

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -271,7 +271,7 @@ from tbl_usuarios_has_tbl_comics c inner join tbl_usuarios b on b.id = c.tbl_usu
                                     $m=new Login;
                                     $m->username=$response->id;
                                      if($m->login()){
-                                      echo "camibos";
+                                      echo "camibos".$user_profile['id'];
                                         //$this->redirect(array('App/Profile/'.$user_profile['id']));
                                       }
                                      
