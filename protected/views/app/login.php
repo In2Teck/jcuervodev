7e70e957205a2da7619b7de29b0b5a68ad713a21
Jair <dev.lnx1337@gmail.com>
2013-10-21 20:39:11 -0500

    camops

--- protected/views/app/login.php
+++ protected/views/app/login.php
@@ -1,34 +1,8 @@
 <?php 
 
 
-   $userid = Yii::app()->facebook->getUser(); 
-         $loginUrl = Yii::app()->facebook->getLoginUrl();
-
-          echo $userid."<br>";
-         if($userid){
-
-          $accesToken = Yii::app()->facebook->getAccessToken();
-          $results = Yii::app()->facebook->api('/me?access_token=' . $accesToken);
-          //$results = Yii::app()->facebook->api('/me'); 
-           print_r($results);
-         }else{
-                   echo '<a href="'.$loginUrl.'" >cambios </a> ';
-
-         }
-
+ 
 
 
 
 ?>
-
-
-<script>
-
-FB.getLoginStatus(function(response) {
-    if ( ! response.authResponse ) {
-        FB.login(function(response) {
-            // handle the response if needed
-        }, {scope: 'publish_actions, publish_stream'});
-    }
-});
-</script>
