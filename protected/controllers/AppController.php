3c3279464e81cd11846e8d0c134da467ae40a239
Jair <dev.lnx1337@gmail.com>
2013-10-21 21:47:42 -0500

    cambios

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -272,9 +272,7 @@ from tbl_usuarios_has_tbl_comics c inner join tbl_usuarios b on b.id = c.tbl_usu
                                     $m->username=$response->id;
                                      if($m->login()){
                                          
-                                          print_r($m);
-                                         echo "camibos".$user_profile['id'];
-                                        //$this->redirect(array('App/Profile/'.$user_profile['id']));
+                                        $this->redirect(array('App/Profile/'.$user_profile['id']));
                                       
 
                                       }
