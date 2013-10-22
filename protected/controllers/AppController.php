bfab1e40f51a1f3c2b6ae9995931fb0ebc8f8181
Jair <dev.lnx1337@gmail.com>
2013-10-21 21:39:09 -0500

    coma

--- protected/controllers/AppController.php
+++ protected/controllers/AppController.php
@@ -263,22 +263,23 @@ from tbl_usuarios_has_tbl_comics c inner join tbl_usuarios b on b.id = c.tbl_usu
                                   }
 
 
-                                    echo $response->isFan;
-                                /*
+                                
                                   
                                   if($response->isFan){
                                     
-                                     
+                                     echo "lamism";
+                                    /*
                                     $m=new Login;
                                     $m->username=$response->id;
                                      if($m->login()){
                                         $this->redirect(array('App/Profile/'.$user_profile['id']));
-                                   }
+                                      }
+                                     */
                                     
                                   }else{
-                                     $this->render('//app/nofan',array('loginUrl'=>$loginUrl));
-                                  }      
-                                  */
+                                 //    $this->render('//app/nofan',array('loginUrl'=>$loginUrl));
+                                   }      
+                                
                                           
                                                          
             
